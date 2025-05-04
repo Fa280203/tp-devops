11b0705cafbd8f016108ae50c1bd51493afb8fc9
Fa280203 <ndeye.samba@efrei.net>
2025-05-04 19:58:00 +0200

    commit

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello CI-CD")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+
+@app.route('/status')
+def status():
+    return "Status: OK"
+
+
+if __name__ == "__main__":
+    app.run()
