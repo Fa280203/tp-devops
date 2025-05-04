9e82ce9d3b5565ac83aec7aae9765cf271084f1a
Fa280203 <ndeye.samba@efrei.net>
2025-05-04 19:32:45 +0200

    Ajout de la route /status

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("hello CI-CD")
+# app . py
+from flask import Flask
+app = Flask ( __name__ )
+@app.route('/')
+def hello () :
+	return " Hello , CI / CD ! "
+if __name__ == "__main__":
+	app . run ()
